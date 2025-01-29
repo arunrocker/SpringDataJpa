f82a50a04aab21748549cba8408c44ba6f6e62c7
arunrocker <m.arunkumarmar12@gmail.com>
2025-01-29 08:38:36 +0530

    Check Hibernate Dependencies for Second lvl

---
+++ src/main/java/com/learnings/springJpa/product/entity/Product.java
@@ -0,0 +1,48 @@
+package com.learnings.springJpa.product.entity;
+
+import java.io.Serializable;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class Product implements Serializable{
+	@Id
+	private int id;
+	private String name;
+	@Column(name="description")
+	private String desc;
+	private Double price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+}
